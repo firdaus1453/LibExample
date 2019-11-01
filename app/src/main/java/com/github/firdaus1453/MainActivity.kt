6aa180fd71a65cbc3f2bd8c0c3376ee3bcff507a
firdaus1453 <mfirdaus1453@gmail.com>
2019-11-01 14:45:54 +0700

    Add some code to library Test call library from modul app

--- app/src/main/java/com/github/firdaus1453/MainActivity.kt
+++ app/src/main/java/com/github/firdaus1453/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.github.firdaus1453
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.lib.LibDebug.Builder.i
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        i("holla")
     }
 }
--- lib/src/main/java/com/github/lib/LibDebug.kt
+++ lib/src/main/java/com/github/lib/LibDebug.kt
@@ -1,7 +1,16 @@
 package com.github.lib
 
+import android.util.Log
+
 /**
  * Created by Muhammad Firdaus on 01/11/2019.
  */
 class LibDebug {
+    companion object Builder {
+        private const val TAG = "LIB_EXAMPLE_ANDROID"
+
+        fun i(message:String){
+            Log.i(TAG, message)
+        }
+    }
 }
